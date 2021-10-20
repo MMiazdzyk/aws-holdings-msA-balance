e755b809f991f02d7251e1814ed2877e00f4e760
Maciej_Miazdzyk <maciej.miazdzyk@refinitiv.com>
2021-10-20 14:44:04 +0200

    initial commit

---
+++ src/main/java/com/holdings/msAbalance/MsABalanceApplication.java
@@ -0,0 +1,13 @@
+package com.holdings.msAbalance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsABalanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsABalanceApplication.class, args);
+	}
+
+}
